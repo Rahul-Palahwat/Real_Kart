88d123304072ac584ce2d9406b9c8612ed350530
Rahul Kumar <rahulkumar@Rahuls-MacBook-Air.local>
2022-06-27 13:42:03 +0530

    Added responsiveness in footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,13 +1,16 @@
-import { Flex, IconButton } from '@chakra-ui/react'
+import { Flex, IconButton, useMediaQuery } from '@chakra-ui/react'
 import React from 'react'
 import {FaInstagram,FaGithub, FaLinkedin} from 'react-icons/fa'
 
 const Footer:React.FC = () => {
+
+  const [isNotSmallerScreen] = useMediaQuery('(min-width:1050px)')
+
   return (
     <div>
         <Flex h={"12vh"} backgroundColor="grey" direction={"column"} >
             <Flex justifyContent={"center"} height="60%" mt="2" alignItems={"center"}>
-                <Flex width={"8%"}>Follow Us :</Flex>
+                <Flex mr={2}>Follow Us :</Flex>
             <IconButton icon={<FaGithub/>} aria-label={'sd'} mr="3"/>
             <IconButton icon={<FaLinkedin/>} aria-label={'sd'} mr="3"/>
             <IconButton icon={<FaInstagram/>} aria-label={'sd'}/>
